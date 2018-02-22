d95fd695d64b60a4439ea51bc1b9af8d849333d3
n-aggarwal <31132760+n-aggarwal@users.noreply.github.com>
2018-02-22 16:32:21 +0530

    Add files via upload

---
+++ PATTERN/Manmohan Loves Patterns- II.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+     int n,i,j;
+     cin>>n;
+     cout<<"1\n";
+     if(n!=1)
+     {
+          for(i=1;i<n;i++)
+          {
+               cout<<i;
+               for(j=1;j<i;j++)
+                    cout<<"0";
+               cout<<i;
+               cout<<"\n";
+          }
+     }
+}
