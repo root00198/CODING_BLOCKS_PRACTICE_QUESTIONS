f6ae0721a3ec9062b493c76412b1574336fe8f6e
prateeknanda10 <51031434+prateeknanda10@users.noreply.github.com>
2020-07-19 12:56:11 +0530

    Update Pascal's Triangle.cpp
    the one that you already had  printed the output wihtout the starting spaces.
    like: for n=4->
    1
    11
    121
    1331
    however, the required output was in this format:
         1
       1 1
     1 2 1
    1 3 3 1

--- PATTERN/Pascal's Triangle.cpp
+++ PATTERN/Pascal's Triangle.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
-
+#include<iomanip>
 using namespace std;
-
-int main()
-{
-     int n,i,j;
-     cin>>n;
-
-     int a[n][n];
-
-     for(i=0;i<n;i++)
-     {
-          for(j=0;j<=i;j++)
-          {
-               if(i==0||j==i||j==0)
-                    a[i][j]=1;
-               else
-                    a[i][j]=a[i-1][j-1]+a[i-1][j];
-               cout<<a[i][j]<<" ";
-          }
-          cout<<"\n";
-     }
+long fact(long n){
+   int i, fact = 1;
+   for(i = n; i>1; i--)
+      fact *= i;
+   return fact;//factorial of given number
 }
+long nCr(long n, long r){
+   long nume = 1, i;
+   for(i = n; i>r; i--)
+      nume *= i;
+   return long(nume/fact(n-r));//generate result of nCr
+}
+void genPascalsTriangle(long n){
+   for(int i = 0; i<n; i++){
+      for(int j = 0; j<(n-i-1); j++)
+         cout <<setw(3)<< " ";//printing space to show triangular form
+      for(int j = 0; j<(i+1); j++)
+         cout <<setw(3)<< nCr(i, j) <<setw(3)<< " ";
+      cout << endl;
+   }
+}
+main(){
+   int n;
+   cin >> n;
+   genPascalsTriangle(n);
+}
+
+
